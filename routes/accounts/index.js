f208da6108ad8673466dbe05fae0bf5e8253db61
xRokz <aboodxgameing@gmail.com>
2020-08-26 04:41:56 +0200

    Cleaning old code and added register and @me to routes

--- routes/accounts/index.js
+++ routes/accounts/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.use("/join", require("./join"));
 router.use("/login", require("./login"));
+router.use("/@me", require("./@me"));
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -9,29 +9,5 @@ router.get('/', function (req, res) {
   res.status(400).json({status: "failed", body: {errors: [{message: "Can't Access This Page."}],}});
 })
 router.use("/accounts", require("./accounts"))
-router.get("/seed",  (req, res) => {
-  let db = mysql.makeConnection();
-  let ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  db.connect();
-
-  let username = "xRokz";
-  let password = "rokz123";
-  let email = "rokz@toolslib.co"
-  db.query(`DELETE FROM users WHERE '1'='1'`);
-  var token = makeToken();
-  bcrypt.hash(password, 10, function(err, hash) {
-      db.query(`INSERT INTO users (username, password, token, created_at, ip, email) VALUES ('${username}', '${hash}', '${token}', '${new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + " " +new Date().getMonth() + "/" + new Date().getDate() + "/" + new Date().getFullYear()}', '${ip}', '${email}')`, (err, rows) => {
-        db.query(`SELECT * FROM users`, (err, rows) => {
-          res.send(rows);
-          db.end();
-        });
-      })
-  });
-
-
-
-
-
-});
 
 module.exports = router;
